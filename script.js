5655d5775cd4053545e30462c624a81402981b8e
FernanEd <fernandoed.bm@gmail.com>
2020-12-10 11:22:15 -0600

    2.0 added controls system

--- script.js
+++ script.js
@@ -1,21 +1,93 @@
 const DISPLAY = document.querySelector("#display");
+const DISPLAY_WIDTH = 480;
 
 let pixelWidth = 16;
+generateGrid();
+
+const PIXEL_CONTROL = document.querySelector("#control-pixel");
 
 
-for(let i = 0; i < pixelWidth; i++){
-    for (let j = 0; j < pixelWidth; j++){
-        const PIXEL = document.createElement("div");
-        PIXEL.classList.add('pixel');
-        DISPLAY.appendChild(PIXEL);
+
+PIXEL_CONTROL.addEventListener("change", e => {
+    if (PIXEL_CONTROL.value >= 10 && PIXEL_CONTROL.value <= 100)
+    {
+        pixelWidth = PIXEL_CONTROL.value;
+        generateGrid();
+    }
+});
+
+function generateGrid(){
+    //Remove old grid
+    DISPLAY.innerHTML = "";
+    //Generate new grid
+    for(let i = 0; i < pixelWidth; i++){
+        for (let j = 0; j < pixelWidth; j++){
+            const PIXEL = document.createElement("div");
+            PIXEL.classList.add('pixel');
+            PIXEL.style.width = `${DISPLAY_WIDTH/pixelWidth}px`;
+            PIXEL.style.height = `${DISPLAY_WIDTH/pixelWidth}px`;
+            DISPLAY.appendChild(PIXEL);
+        }
+    }
+
+    //Make coloring work
+    coloring();
+
+}
+
+// CONTROLS
+let selectedControl = 0; // 0 Pen - 1 Color pencil - 2 Rainbow
+
+const PEN_CONTROL = document.querySelector("#control-pen");
+const PENCIL_CONTROL = document.querySelector("#control-color");
+const RAINBOW_CONTROL = document.querySelector("#control-rainbow");
+const CLEAR_CONTROL = document.querySelector("#control-clear");
+
+PEN_CONTROL.addEventListener("click", selectControl);
+PENCIL_CONTROL.addEventListener("click", selectControl);
+RAINBOW_CONTROL.addEventListener("click", selectControl);
+CLEAR_CONTROL.addEventListener("click", generateGrid);
+
+function selectControl(){
+    //Toggle selectability
+    if (Array.from(this.classList).includes('selectable'))
+    {
+        deselectOthers();
+        this.classList.add('selected');
+        this.classList.remove('selectable');
+    }
+
+    //Update control variable
+    switch(this.id){
+        case 'control-pen':
+            selectControl = 0;
+            break;
+        case 'control-color':
+            selectControl = 1;
+            break;
+        case 'control-rainbow':
+            selectControl = 2;
+            break;
     }
 }
 
+function deselectOthers(){
+    const CONTROLS = [PEN_CONTROL, PENCIL_CONTROL, RAINBOW_CONTROL];
 
-const PEN_CONTROL = document.querySelector("#control-pen");
+    CONTROLS.forEach(control => {
+        control.classList.remove('selected');
+        control.classList.add('selectable');
+    })
+}
 
-const PENCIL_CONTROL = document.querySelector("#control-pencil");
 
-const RAINBOW_CONTROL = document.querySelector("#control-rainbow");
+// COLORING
+function coloring(){
+    const PIXELS = document.querySelectorAll('.pixel');
 
-const CLEAR_CONTROL = document.querySelector("#control-clear");
+    PIXELS.forEach(pixel => {
+        pixel.addEventListener("mouseover", e => {
+            pixel.style.backgroundColor = "red";
+        })
+    })
+}
