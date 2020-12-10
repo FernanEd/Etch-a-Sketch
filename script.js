00c3adc7dcd8c35a5c9d4303dd360bf690d70aa1
FernanEd <fernandoed.bm@gmail.com>
2020-12-09 22:33:12 -0600

    1.0 gui done

---
+++ script.js
@@ -0,0 +1,23 @@
+const DISPLAY = document.querySelector("#display");
+
+let pixelWidth = 16;
+
+
+for(let i = 0; i < pixelWidth; i++){
+    for (let j = 0; j < pixelWidth; j++){
+        const PIXEL = document.createElement("div");
+        PIXEL.classList.add('pixel');
+        DISPLAY.appendChild(PIXEL);
+    }
+}
+
+
+
+
+const PEN_CONTROL = document.querySelector("#control-pen");
+
+const PENCIL_CONTROL = document.querySelector("#control-pencil");
+
+const RAINBOW_CONTROL = document.querySelector("#control-rainbow");
+
+const CLEAR_CONTROL = document.querySelector("#control-clear");
